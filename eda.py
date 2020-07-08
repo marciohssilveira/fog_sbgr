5cc99accfa94b905ea53df6e8b44218e715095f9
Marcio Silveira <mhsilveira.br@gmail.com>
2020-07-08 16:42:55 -0300

    clustering and fog type definition

--- eda.py
+++ eda.py
@@ -1,14 +1,19 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-fog = pd.read_csv('./data/labeled_fog.csv')
+fog = pd.read_csv('./data/labeled_fog.csv', index_col='DATE')
+fog.index = pd.to_datetime(fog.index)
 
-print(fog.info())
+hourly_count = fog.pivot_table(values='temperature', index=fog.index.hour, aggfunc=np.count_nonzero)
+monthly_count = fog.pivot_table(values='temperature', index=fog.index.month, aggfunc=np.count_nonzero)
+yearly_count = fog.pivot_table(values='temperature', index=fog.index.year, aggfunc=np.count_nonzero)
 
-fog['DATE'] = pd.to_datetime(fog['DATE'])
+hourly_count_label = fog.pivot_table(values='temperature', index=fog.index.hour, columns='label', aggfunc=np.count_nonzero)
+monthly_count_label = fog.pivot_table(values='temperature', index=fog.index.month, columns='label', aggfunc=np.count_nonzero)
+yearly_count_label = fog.pivot_table(values='temperature', index=fog.index.year, columns='label', aggfunc=np.count_nonzero)
 
-fog.groupby(pd.Grouper(key='DATE', freq='h'))['temperature'].mean()
-
-# next step: pivot tables
+hourly_mean_temperature = fog.pivot_table(values='temperature', index=fog.index.hour, columns='label', aggfunc=np.mean)
+hourly_mean_visibility = fog.pivot_table(values='visibility', index=fog.index.hour, columns='label', aggfunc=np.mean)
--- feature_engineering.py
+++ feature_engineering.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from scipy.cluster.hierarchy import dendrogram, ward
+from sklearn.preprocessing import StandardScaler
 
 sbgr = pd.read_csv('./data/sbgr_data.csv')
 """
@@ -14,29 +15,31 @@ Dataset columns:
 
 
 # Create column for fog occurrence
-mask = sbgr['visibility'] < 1000
+mask = sbgr.filter(like='phenomenon').applymap(lambda L: 'Fog' in L).any(axis=1)
 fog = sbgr[mask]
 
 # Drop some columns
 fog.drop(['phenomenon_1', 'phenomenon_2', 'phenomenon_3',
-          'coverage_2', 'coverage_3', 'coverage_4',
-          'coverage_1', 'ceiling'], axis=1, inplace=True)
+          'coverage_2', 'coverage_3', 'coverage_4'], axis=1, inplace=True)
 
 # Fill missing values
-# fog['coverage_1'] = fog['coverage_1'].fillna(0)  # the absence of coverage means 0 oktas
-# fog['ceiling'] = fog['ceiling'].fillna(5000)  # the absence of ceiling means above 5000 feet
+fog['coverage_1'] = fog['coverage_1'].fillna(0)  # the absence of coverage means 0 oktas
+fog['ceiling'] = fog['ceiling'].fillna(5000)  # the absence of ceiling means above 5000 feet
 fog['temperature'] = fog['temperature'].fillna(method='ffill')
 fog['dew'] = fog['dew'].fillna(method='ffill')
 fog['slp'] = fog['slp'].fillna(method='ffill')
 fog['rh'] = fog['rh'].fillna(method='ffill')
 
+# Create a variable to encapsulate the 12 hour advection of humidity
+
 
 # Separating fog types
 # Preparing data for clustering
-print(fog.info())
 
+X = fog.drop(['DATE'], axis=1)
 
-X = fog.drop('DATE', axis=1)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
 # Visualizing hierarchy
 Z = ward(X)
@@ -44,7 +47,7 @@ dendrogram(Z)
 plt.show() # 4 clusters are a good split
 
 # Clustering
-clustering = KMeans(n_clusters=4)
+clustering = KMeans(n_clusters=3)
 clustering.fit(X)
 fog['label'] = clustering.labels_
 
