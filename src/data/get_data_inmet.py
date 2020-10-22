9e56d161f3b52aae583354f889d6d286989cfbd4
Marcio Silveira <marcio@MacBookPro.local>
2020-10-22 19:08:55 -0300

    updated tree

--- src/data/get_data_inmet.py
+++ src/data/get_data_inmet.py
@@ -1,65 +1,145 @@
 import pandas as pd
 
-inmet_data = pd.read_csv('./data/dados_A701_H_2006-07-24_2020-07-01.csv', sep=';', skiprows=10)
-inmet_data = inmet_data.drop(inmet_data.columns[-1], axis=1)
+import urllib.request
+from urllib.error import HTTPError
+import pandas as pd
+from pandas.io import parsers
+import os
+import math
+import re
+import numpy as np
+from pathlib import Path
+import warnings
+import datetime
+import zipfile
+import shutil
 
-inmet_data = inmet_data[['Data Medição',
-                         'Hora Medição',
-                         'PRECIPITAÇÃO TOTAL, HORÁRIO(mm)',
-                         'RADIACAO GLOBAL(W/m²)']]
+warnings.filterwarnings('ignore')
 
-inmet_data.columns = ['date',
-                      'time',
-                      'precipitation',
-                      'radiation']
 
-inmet_data['date_time'] = inmet_data['date'].astype(str) + \
-                          ' ' + \
-                          inmet_data['time'].apply(lambda x: str(x).zfill(4))
+class GetInmetData:
+    """
+    Contain functions to gather data from ISD,
+    organise the files in directories and
+    extract information from them
+    """
 
-inmet_data = inmet_data[['date_time',
-                         'precipitation',
-                         'radiation']]
+    def __init__(self):
+        self.station = 'SAO PAULO - MIRANTE'
+        self.end_year = datetime.datetime.today().year - 1
+        self.start_year = datetime.datetime.today().year - 10
 
-isd_data = pd.read_csv('./data/isd_data.csv')
-isd_data.columns = ['date_time',
-                    'direction',
-                    'speed',
-                    'visibility',
-                    'phenomenon_1',
-                    'phenomenon_2',
-                    'phenomenon_3',
-                    'coverage_1',
-                    'coverage_2',
-                    'coverage_3',
-                    'coverage_4',
-                    'ceiling',
-                    'temperature_isd',
-                    'dew_isd',
-                    'slp',
-                    'rh']
+    def download_inmet_data(self):
+        """
+        Creates the link to download ISD files as well as the directories to put the files
+        """
+        print(f'Downloading {self.station} data')
+        for year in range(self.start_year, self.end_year, 1):
+            url = f'https://portal.inmet.gov.br/uploads/dadoshistoricos/{year}.zip'
+            Path(f'data/raw/{self.station}/inmet/zip/').mkdir(parents=True, exist_ok=True)
+            zip_filename = f'data/raw/{self.station}/inmet/zip/{year}.zip'
+            # Only download if file does not exist
+            if not os.path.exists(zip_filename):
+                try:
+                    urllib.request.urlretrieve(url, zip_filename)
+                except urllib.error.HTTPError as exception:
+                    print(f'Unfortunately there is no {year} data available'
+                          f' for {self.station}: Error {exception.code}')
+                    continue
+            Path(f'data/raw/{self.station}/inmet/csv/').mkdir(parents=True, exist_ok=True)
+            csv_filepath = f'data/raw/{self.station}/inmet/csv/'
+            # # Only unzip if file does not exist
+            # if not os.path.exists(_filename):
+            with zipfile.ZipFile(zip_filename, 'r') as zip:
+                files = zip.namelist()
+                regex = re.compile(f'^(?=.*({self.station}))(?!.*box).*$')
+                for file in files:
+                    if regex.match(file):
+                        # Extract a single file from zip
+                        # zip.extract(file, csv_filepath)
+                            # open the entry so we can copy it
+                        member = zip.open(file)
+                        
+                        with open(os.path.join(csv_filepath, os.path.basename(file)), 'wb') as outfile:
+                            # copy it directly to the output directory,
+                            # without creating the intermediate directory
+                            shutil.copyfileobj(member, outfile)
 
-isd_data = isd_data[['date_time',
-                     'direction',
-                     'speed',
-                     'visibility',
-                     'phenomenon_1',
-                     'temperature_isd',
-                     'dew_isd',
-                     'rh',
-                     'slp', ]]
+        print('Download complete.')
+        data = self.process_files()
+        data.to_csv(f'data/interim/{self.station}_inmet_data.csv')
+        print('Done')
+        data.index = pd.to_datetime(data.index)
+        return data
 
-inmet_data['date_time'] = pd.to_datetime(inmet_data['date_time'])
+    def process_files(self):
+        """
+        Takes all the raw downloaded files and unites them into one file
+        """
+        csv_list = os.listdir(path=f'data/raw/{self.station}/inmet/csv/')
 
-inmet_data.to_csv('./data/inmet_data.csv', index=False)
+        grouped = []
+        for file in sorted(csv_list):
+            # DATE column is used as index
+            # try:
+            inmet_data = pd.read_csv(file, sep=';', skiprows=8, encoding='latin_1')
+            inmet_data = inmet_data.drop(inmet_data.columns[-1], axis=1)
+            inmet_data = inmet_data[['DATA (YYYY-MM-DD)', 'HORA (UTC)',
+                                        'PRECIPITAÇÃO TOTAL, HORÁRIO (mm)', 'RADIACAO GLOBAL (KJ/m²)']]
+            inmet_data.columns = ['date', 'time',
+                                    'precipitation', 'radiation']
+            inmet_data['date_time'] = inmet_data['date'].astype(
+                str) + ' ' + inmet_data['time'].apply(lambda x: str(x).zfill(4))
+            inmet_data = inmet_data[[
+                'date_time', 'precipitation', 'radiation']]
+            # except:
+            #     f'{file} data for {self.station} could not be processed.'
+            #     continue
+            grouped.append(inmet_data)
+        # Stores all data data into a dataframe
+        data = pd.concat(grouped, sort=False)
+        data = data.set_index(data.index)  # Sets index to datetime format
+        return data
 
-isd_data['date_time'] = pd.to_datetime(isd_data['date_time'])
+      #   isd_data = pd.read_csv('./data/isd_data.csv')
+      #   isd_data.columns = ['date_time',
+      #                       'direction',
+      #                       'speed',
+      #                       'visibility',
+      #                       'phenomenon_1',
+      #                       'phenomenon_2',
+      #                       'phenomenon_3',
+      #                       'coverage_1',
+      #                       'coverage_2',
+      #                       'coverage_3',
+      #                       'coverage_4',
+      #                       'ceiling',
+      #                       'temperature_isd',
+      #                       'dew_isd',
+      #                       'slp',
+      #                       'rh']
 
-data = isd_data.merge(inmet_data, on='date_time', how='left')
+      #   isd_data = isd_data[['date_time',
+      #                        'direction',
+      #                        'speed',
+      #                        'visibility',
+      #                        'phenomenon_1',
+      #                        'temperature_isd',
+      #                        'dew_isd',
+      #                        'rh',
+      #                        'slp', ]]
 
-data[['precipitation',
-      'radiation']] = (data[['precipitation',
-                             'radiation']].ffill() + data[['precipitation',
-                                                           'radiation']].bfill()) / 2
+      #   inmet_data['date_time'] = pd.to_datetime(inmet_data['date_time'])
 
-data.to_csv('./data/consolidated_data.csv', index=False)
+      #   inmet_data.to_csv('./data/inmet_data.csv', index=False)
+
+      #   isd_data['date_time'] = pd.to_datetime(isd_data['date_time'])
+
+      #   data = isd_data.merge(inmet_data, on='date_time', how='left')
+
+      #   data[['precipitation',
+      #         'radiation']] = (data[['precipitation',
+      #                                'radiation']].ffill() + data[['precipitation',
+      #                                                              'radiation']].bfill()) / 2
+
+      #   data.to_csv('./data/consolidated_data.csv', index=False)
